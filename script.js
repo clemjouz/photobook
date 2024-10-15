517db147a119618efd6a9b14a82efbbe2c91b230
clemjouz <clement.jouzeau@etu.univ-poitiers.fr>
2024-10-15 16:36:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const galleryLinks = document.querySelectorAll('.gallery a'); // Sélectionne les liens autour des images
+    galleryLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault(); // Empêche le comportement par défaut du lien
+
+            // Ouvre le lien dans un nouvel onglet
+            window.open(link.href, '_blank'); 
+        });
+    });
+
+    // Si tu veux garder le comportement de l'overlay pour voir les images agrandies, garde le code existant ici
     const galleryImages = document.querySelectorAll('.gallery img');
     galleryImages.forEach(image => {
         image.addEventListener('click', () => {
