4bc1832f8dbcca7b2db713ab98c424142fdb3914
clemjouz <clement.jouzeau@etu.univ-poitiers.fr>
2024-11-26 14:36:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function closeSidebar() {
 // Fonction pour gérer le défilement entre les slides
 function scrollSlide(direction) {
     const slides = document.querySelectorAll('.slide');
-    const currentSlide = Math.floor(window.scrollY / window.innerHeight);
+    const currentSlide = Math.round(window.scrollY / window.innerHeight);
     const nextSlide = currentSlide + direction;
 
     // On empêche de sortir des bornes
@@ -21,22 +21,12 @@ function scrollSlide(direction) {
             top: nextSlide * window.innerHeight,
             behavior: 'smooth'
         });
-
-        // Ajouter la classe 'show' pour l'animation d'apparition
-        slides.forEach((slide, index) => {
-            if (index === nextSlide) {
-                slide.classList.add('show');
-            } else {
-                slide.classList.remove('show');
-            }
-        });
     }
 }
 
 // Activer l'animation lors du chargement des slides
 document.addEventListener('DOMContentLoaded', function () {
-    const slides = document.querySelectorAll('.slide');
-    slides[0].classList.add('show'); // Afficher la première slide
+    // Aucune animation nécessaire, toutes les slides sont empilées verticalement.
 });
 
 // Ajouter les événements pour les touches fléchées
