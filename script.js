d03b76dd252a3f06904da48915cebe656058167c
clemjouz <clement.jouzeau@etu.univ-poitiers.fr>
2024-11-26 15:48:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// Fonction pour ouvrir le menu latéral
-function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.style.width = sidebar.style.width === '250px' ? '0' : '250px';
-}
-
-// Fonction pour fermer le menu latéral
-function closeSidebar() {
-    document.getElementById('sidebar').style.width = '0';
-}
-
 // Fonction pour gérer le défilement entre les slides
 function scrollSlide(direction) {
     const slides = document.querySelectorAll('.slide');
@@ -70,7 +59,7 @@ document.addEventListener('touchmove', (e) => {
     }
 }, { passive: false });
 
-// Gestion du redimensionnement de la fenêtre
+// Redimensionnement de la fenêtre
 window.addEventListener('resize', function() {
     const slides = document.querySelectorAll('.slide');
     slides.forEach(slide => {
